eb43922aac478ed831d462b3b2fcc8100ababd17
hubvue <cckcc.kim@gmail.com>
2022-03-13 16:06:56 +0800

    feat(utils): add env methods

---
+++ packages/utils/__mocks__/ua.ts
@@ -0,0 +1,15 @@
+export const browserUA =
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+export const alipayUA =
+  'Mozilla/5.0 (Linux; U; Android 10; zh-CN; H9493 Build/52.1.A.3.49) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 UWS/3.21.0.169 Mobile Safari/537.36 AlipayChannelId/5136 UCBS/3.21.0.169_200731162109 NebulaSDK/1.8.100112 Nebula AlipayDefined(nt:WIFI,ws:411|0|3.5,ac:sp) AliApp(AP/10.2.0.8026) AlipayClient/10.2.0.8026 Language/zh-Hans useStatusBar/true isConcaveScreen/false Region/CN NebulaX/1.0.0 Ariver/1.0.0'
+
+export const wechatUA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Mobile/10B329 MicroMessenger/5.0.1'
+export const androidUA = 'Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36'
+
+export const douyinUA = 'Mozilla/5.0 (Linux; Android 9; COL-TL10 Build/HUAWEICOL-TL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.156 Mobile Safari/537.36 aweme_130400 JsSdk/1.0 NetType/WIFI Channel/huawei_1 AppName/aweme app_version/13.4.0 ByteLocale/zh-Hans-CN Region/CN AppSkin/black AppTheme/dark TTWebView/0751130003508'
+export const iosUA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13E230/myapp/1.0.0'
+export const ipadUA = 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10'
+export const qqUA = 'Mozilla/5.0 (Linux; Android 5.1; OPPO R9tm Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043128 Safari/537.36 V1_AND_SQ_7.0.0_676_YYB_D PA QQ/7.0.0.3135 NetType/4G WebP/0.3.0 Pixel/1080'
+export const wxworkUA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92 wxwork/2.4.2 MicroMessenger/6.3.22 Language/zh'
+export const windowsUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+export const macosUA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43'
--- packages/utils/src/env.ts
+++ packages/utils/src/env.ts
@@ -21,7 +21,52 @@ export const isSpecificPlatform = (platformRegexp: RegExp) => {
  */
 export const isBrowser = () => typeof window !== 'undefined'
 /**
- * Determining whether the platform environment is a alipay
+ * @description Determining whether the platform environment is a alipay
  * @returns 
  */
 export const isAlipay = () => isSpecificPlatform(/AlipayClient/i)
+/**
+ * @description Determining whether the platform environment is a wechat
+ * @returns 
+ */
+export const isWechat = () => isSpecificPlatform(/MicroMessenger/i)
+/**
+ * @description Determining whether the platform environment is a corporate weChat
+ * @returns 
+ */
+export const isWechatWork = () => isSpecificPlatform(/wxwork/i)
+/**
+ * @description Determining whether the platform environment is a QQ
+ * @returns 
+ */
+export const isQQ = () => isSpecificPlatform(/QQ/i)
+/**
+ * @description Determining whether the platform environment is a douyin
+ * @returns 
+ */
+export const isDouyin = () => isSpecificPlatform(/aweme/i)
+/**
+ * @description Determining whether the platform environment is a android
+ * @returns 
+ */
+export const isAndroid = () => isSpecificPlatform(/Android/i)
+/**
+ * @description Determining whether the platform environment is a ios
+ * @returns 
+ */
+export const isIos = () => isSpecificPlatform(/iphone|ipad|ipod|ios/i)
+/**
+ * @description Determining whether the platform environment is a ipad
+ * @returns 
+ */
+export const isPad = () => isSpecificPlatform(/ipad/i)
+/**
+ * @description Determining whether the platform environment is a windows
+ * @returns 
+ */
+export const isWindows = () => isSpecificPlatform(/Windows/i)
+/**
+ * @description Determining whether the platform environment is a macos
+ * @returns 
+ */
+export const isMacos = () => isSpecificPlatform(/Macintosh|Mac/i)

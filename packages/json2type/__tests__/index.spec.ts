bcff5ea2d803359909c11e7cd81c0219b3f82eda
hubvue <cckcc.kim@gmail.com>
2022-03-22 21:08:16 +0800

    test(json2type): update tests

--- packages/json2type/__tests__/index.spec.ts
+++ packages/json2type/__tests__/index.spec.ts
@@ -1,10 +1,3 @@
-import { json2type } from '../src'
-import { resolve } from 'path'
 describe('json2type', () => {
-  // test('1', () => {
-  //   const result = json2type({
-  //     input: resolve(__dirname, './tmp.json')
-  //   })
-  //   console.log('result', result)
-  // })
+  
 })

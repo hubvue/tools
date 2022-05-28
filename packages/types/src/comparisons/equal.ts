327bcda96304c3530db0fd152e6a5e666b497635
hubvue <cckcc.kim@gmail.com>
2022-05-28 15:36:32 +0800

    refactor: update

--- packages/types/src/comparisons/equal.ts
+++ packages/types/src/comparisons/equal.ts
@@ -7,10 +7,3 @@ export type Equal<A extends unknown, B extends unknown> =
       ? true
       : false
     : false
-
-/**
- * @description Determining that two types are strictly equal
- */
-export type StrictlyEqual<X extends unknown, Y extends unknown> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2)
-  ? true
-  : false
